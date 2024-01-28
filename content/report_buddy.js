e79ea30a79609d3089e86b6b6c1c6ec725e9d98d
NiTrO0FuN <128875940+NiTrO0FuN@users.noreply.github.com>
2024-01-28 14:05:08 +0100

    Add TTT report template

--- content/report_buddy.js
+++ content/report_buddy.js
@@ -3,16 +3,18 @@ let rules = {}
 chrome.storage.local.get("rules").then(async r => {
     let storedRules = r.rules
     if(!storedRules || (Date.now() - storedRules.lastUpdate > 1*60*60*1000)) {
-        storedRules = await fetchRules()
+        storedRules = {lastUpdate: Date.now()}
+        storedRules.darkrp = await fetchRules("darkrp")
+        storedRules.ttt = await fetchRules("ttt")
         chrome.storage.local.set({rules: storedRules})
     }
     rules = storedRules
 })
 
-async function fetchRules() {
-    const newRules = {lastUpdate: Date.now()}
+async function fetchRules(serverType) {
+    const newRules = {}
     let parser = new DOMParser();
-    const result = await (await fetch("https://nxserv.gg/help/rules/darkrp")).text()
+    const result = await (await fetch("https://nxserv.gg/help/rules/"+serverType)).text()
     const rules_html = parser.parseFromString(result, "text/html");
     const sectionsDiv = rules_html.getElementsByClassName("gamerules")[0].querySelectorAll(":scope > div")
 
@@ -33,7 +35,12 @@ async function fetchRules() {
     return newRules
 }
 
-function writeReportTemplate(dialog, brokenRules) {
+function writeReportTemplate(serverType, dialog, brokenRules) {
+    if(serverType === "darkrp"){writeDarkRPReportTemplate(dialog, brokenRules)}
+    else if(serverType === "ttt"){writeTTTReportTemplate(dialog, brokenRules)}
+}
+
+function writeDarkRPReportTemplate(dialog, brokenRules) {
     const tabs = document.getElementsByClassName("react-tabs")[0]
 
     const title = document.querySelector("input[name='title']")
@@ -45,7 +52,7 @@ function writeReportTemplate(dialog, brokenRules) {
             let brokenRulesText = ""
             for(const ruleNbr in brokenRules[section][subSection]) {
                 if(!brokenRules[section][subSection][ruleNbr]) {continue}
-                brokenRulesText += `\n${parseInt(ruleNbr)+1}. ${rules[section][subSection][ruleNbr]}`
+                brokenRulesText += `\n${parseInt(ruleNbr)+1}. ${rules.darkrp[section][subSection][ruleNbr]}`
             }
             if(brokenRulesText == ""){continue}
             brokenRulesQuotes.push(`[h1]${section}[/h1]\n[b]${subSection}[/b]\n${brokenRulesText}`)
@@ -72,7 +79,46 @@ function writeReportTemplate(dialog, brokenRules) {
     dialog.close()
 }
 
-function askForBrokenRules() {
+function writeTTTReportTemplate(dialog, brokenRules) {
+    const tabs = document.getElementsByClassName("react-tabs")[0]
+
+    const title = document.querySelector("input[name='title']")
+    if(title.value!=chrome.i18n.getMessage("report_title")){title.addAtCaret(chrome.i18n.getMessage("report_title"))}
+
+    let brokenRulesQuotes = []
+    for(const section in brokenRules) {
+        for(const subSection in brokenRules[section]) {
+            let brokenRulesText = ""
+            for(const ruleNbr in brokenRules[section][subSection]) {
+                if(!brokenRules[section][subSection][ruleNbr]) {continue}
+                brokenRulesText += `\n${parseInt(ruleNbr)+1}. ${rules.ttt[section][subSection][ruleNbr]}`
+            }
+            if(brokenRulesText == ""){continue}
+            brokenRulesQuotes.push(`[h1]${section}[/h1]\n[b]${subSection}[/b]\n${brokenRulesText}`)
+        }
+    }
+
+    let brokenRulesToPrint = ""
+    for(const quote of brokenRulesQuotes) {
+        brokenRulesToPrint += `[quote]${quote}[/quote]\n`
+    }
+
+
+    const report =
+`[h1]${chrome.i18n.getMessage("report_name")}:[/h1]\n
+[h1]${chrome.i18n.getMessage("report_steamid")}:[/h1]\n
+[h1]${chrome.i18n.getMessage("report_date")}:[/h1]\n${chrome.i18n.getMessage("dateString", new Date().toLocaleString().split(" "))}\n
+[h1]${chrome.i18n.getMessage("report_location")}:[/h1]\n
+[h1]${chrome.i18n.getMessage("report_brokenrules")}:[/h1]\n${brokenRulesToPrint}[h1]${chrome.i18n.getMessage("report_proofs")}:[/h1]\n
+`
+
+    const textarea = tabs.getElementsByTagName("textarea")[0]
+    if(textarea) {textarea.addAtCaret(report)} 
+
+    dialog.close()
+}
+
+function askForBrokenRules(serverType) {
     const dialog = document.createElement("dialog")
     dialog.classList.add("brokenRules")
 
@@ -81,21 +127,19 @@ function askForBrokenRules() {
     dialog.appendChild(instructions)
 
     const brokenRules = {}
-    for(const section in rules) {
-        if(section === "lastUpdate"){continue}
-
+    for(const section in rules[serverType]) {
         brokenRules[section] = {}
         let title = document.createElement("h2")
         title.innerText = section
         dialog.appendChild(title)
 
-        for(const subSection in rules[section]) {
+        for(const subSection in rules[serverType][section]) {
             brokenRules[section][subSection] = {}
             title = document.createElement("h3")
             title.innerText = subSection
             dialog.appendChild(title)
             const oList = document.createElement("ol")
-            rules[section][subSection].forEach((rule, i) => {
+            rules[serverType][section][subSection].forEach((rule, i) => {
                 const element = document.createElement("li")
                 element.innerText = rule
                 element.addEventListener("click", () => {
@@ -114,7 +158,7 @@ function askForBrokenRules() {
     const validateBtn = document.createElement("btn")
     validateBtn.className = "join button" //Hijacking nx styles
     validateBtn.innerText = chrome.i18n.getMessage("validate")
-    validateBtn.addEventListener("click", () => writeReportTemplate(dialog, brokenRules))
+    validateBtn.addEventListener("click", () => writeReportTemplate(serverType, dialog, brokenRules))
     validateDiv.appendChild(validateBtn)
     dialog.appendChild(validateDiv)
     document.body.appendChild(dialog)
@@ -135,11 +179,17 @@ function addReportBuddy() {
     icon.width = 32
     reportBuddy.appendChild(icon)
 
-    const button = document.createElement("btn")
-    button.classList.add("button")
-    button.innerText = chrome.i18n.getMessage("reportBuddyBtn")
-    button.addEventListener("click", askForBrokenRules)
-    reportBuddy.appendChild(button)
+    const button_darkrp = document.createElement("btn")
+    button_darkrp.classList.add("button")
+    button_darkrp.innerText = chrome.i18n.getMessage("reportBuddyBtnDarkRP")
+    button_darkrp.addEventListener("click", () => askForBrokenRules("darkrp"))
+    reportBuddy.appendChild(button_darkrp)
+
+    const button_ttt = document.createElement("btn")
+    button_ttt.classList.add("button")
+    button_ttt.innerText = chrome.i18n.getMessage("reportBuddyBtnTTT")
+    button_ttt.addEventListener("click", () => askForBrokenRules("ttt"))
+    reportBuddy.appendChild(button_ttt)
 
     form.insertBefore(reportBuddy, tabs)
     form.insertBefore(document.createElement("br"), tabs)
