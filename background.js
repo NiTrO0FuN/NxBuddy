5537b20c1b9d5873fa2ca85757b63fa99f5ddc5d
NiTrO0FuN <alboune@icloud.com>
2024-02-23 23:04:49 +0100

    Add search buddy

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 chrome.webRequest.onCompleted.addListener((details) => {
         const parsedUrl = new URL(details.url);
         if(parsedUrl.pathname.startsWith("/api/online")) {return}
+        else if(parsedUrl.pathname === "/api/forum") {
+            chrome.tabs.sendMessage(details.tabId, "searchBuddy")
+            return
+        }
         chrome.tabs.sendMessage(details.tabId, "onlineStatus")
     },
     { urls: ['*://nxserv.gg/api/*']}
